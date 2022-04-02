dbd7702a2f4c1f358bfab2d8d53a754b89b8f76f
Jonas Lagoni <jonas-lt@live.dk>
2022-04-02 20:41:23 +0200

    refactor: changed bundle behavior and removed unused API

--- documents/bundle-documents.js
+++ documents/bundle-documents.js
@@ -1,10 +1,9 @@
+// Because the bundler does not work outside of the working directory we have to keep this package here for now: https://github.com/asyncapi/bundler/issues/35
 const bundler = require('@asyncapi/bundler');
-const {parse, AsyncAPIDocument} = require('@asyncapi/parser');
 const fs = require('fs');
 const path = require('path');
 
 async function bundleDocuments(filePath, outputFile) {
-
   const fullPath = path.resolve(filePath);
   const content = fs.readFileSync(fullPath, 'utf-8');
   const fileContent = JSON.parse(content);
@@ -13,5 +12,5 @@ async function bundleDocuments(filePath, outputFile) {
   );
   fs.writeFileSync(outputFile, bundledDocument.string());
 };
-bundleDocuments('./rust_server.asyncapi.json', './rust_server_bundle.asyncapi.json');
-bundleDocuments('./rust_processor.asyncapi.json', './rust_processor_bundle.asyncapi.json');
+bundleDocuments('./rust_server.asyncapi.json', '../bundled/rust_server.asyncapi.bundled.json');
+bundleDocuments('./rust_processor.asyncapi.json', '../bundled/rust_processor.asyncapi.bundled.json');
