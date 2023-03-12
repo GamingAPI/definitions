186e288dd4183c97262a34b316f37cd5380bb330
jonaslagoni <jonas-lt@live.dk>
2023-03-12 04:48:49 +0100

    moved files around

--- documents/bundle-documents.js
+++ documents/bundle-documents.js
@@ -3,15 +3,28 @@ const bundler = require('@asyncapi/bundler');
 const fs = require('fs');
 const path = require('path');
 const asyncFile = process.env.asyncFile;
+const filesToBundle = [];
+
+if(asyncFile) {
+  filesToBundle.push(asyncFile);
+} else {
+  filesToBundle.push('rust');
+  filesToBundle.push('rust_public');
+}
 
 async function bundleDocuments(filePath, outputFile) {
   const fullPath = path.resolve(filePath);
   const content = fs.readFileSync(fullPath, 'utf-8');
   const fileContent = JSON.parse(content);
   const bundledDocument = await bundler(
-    [fileContent]
+    [fileContent], 
+    {
+      referenceIntoComponents: false,
+    }
   );
   fs.writeFileSync(outputFile, bundledDocument.string());
   console.log(`Done bundling ${fullPath}`);
 };
-bundleDocuments(`./${asyncFile}.asyncapi.json`, `../bundled/${asyncFile}.asyncapi.json`);
+for (const file of filesToBundle) {
+  bundleDocuments(`./${file}.asyncapi.json`, `../bundled/${file}.asyncapi.json`);
+}
